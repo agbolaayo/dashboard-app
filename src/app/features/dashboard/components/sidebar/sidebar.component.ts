31aa4de61e414f58d1b831ec0a77667484b8e3c8
Agbola Ayobami <me@ayoagbola.com.ng>
2025-05-25 08:13:06 +0100

    fix: fixed responsive related issues

--- src/app/features/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/features/dashboard/components/sidebar/sidebar.component.ts
@@ -10,10 +10,10 @@ import { ThemeService } from '../../../../core/services/theme.service';
 })
 export class SidebarComponent {
   topMenuItems: MenuItem[] = [
-    { id: 'top1', label: 'Lorem', iconPath: ImagePaths.MENU_7, link: '/', active: true},
+    { id: 'top1', label: 'Lorem', iconPath: ImagePaths.MENU_7, link: '/'},
     { id: 'top2', label: 'Lorem', iconPath: ImagePaths.MENU_3, link: '/' },
     { id: 'top3', label: 'Lorem', iconPath: ImagePaths.MENU_1, link: '/' },
-    { id: 'top4', label: 'Lorem', iconPath: ImagePaths.MENU_ACTIVE, link: '/' },
+    { id: 'top4', label: 'Lorem', iconPath: ImagePaths.MENU_ACTIVE, link: '/', active: true },
     { id: 'top5', label: 'Lorem', iconPath: ImagePaths.MENU_5, link: '/' },
     { id: 'top6', label: 'Lorem', iconPath: ImagePaths.MENU_6, link: '/' },
     { id: 'top7', label: 'Lorem', iconPath: ImagePaths.MENU_2, link: '/' },
